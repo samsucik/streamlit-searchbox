cda55591ab3817c5b54744866d8448a34d507030
samsucik <s.sucik@rasa.com>
2024-02-26 08:07:53 +0100

    Add previously made minor changes to the app and its requirements

--- streamlit_searchbox/__init__.py
+++ streamlit_searchbox/__init__.py
@@ -12,10 +12,16 @@ import streamlit.components.v1 as components
 # point to build directory
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.join(parent_dir, "frontend/build")
-_get_react_component = components.declare_component(
-    "searchbox",
-    path=build_dir,
-)
+if os.path.exists(build_dir):
+    _get_react_component = components.declare_component(
+        "searchbox",
+        path=build_dir,
+    )
+else:
+    _get_react_component = components.declare_component(
+        "searchbox",
+        url="http://localhost:3001"
+    )
 
 logger = logging.getLogger(__name__)
 
@@ -51,7 +57,6 @@ def _process_search(
 
     st.session_state[key]["search"] = searchterm
     search_results = search_function(searchterm)
-
     if search_results is None:
         search_results = []
 
@@ -79,11 +84,14 @@ def _process_search(
 @wrap_inactive_session
 def st_searchbox(
     search_function: Callable[[str], List[any]],
-    placeholder: str = "Search ...",
-    label: str = None,
-    default: any = None,
-    clear_on_submit: bool = False,
-    key: str = "searchbox",
+    placeholder: str="Search ...",
+    label: str=None,
+    default: any=None,
+    clear_on_submit: bool=False,
+    key: str="searchbox",
+    on_change: Callable[[any], None]=None,
+    initial_options: List[any]=[],
+    is_required=False,
     **kwargs,
 ) -> any:
     """
@@ -121,18 +129,44 @@ def st_searchbox(
             "options": [],
         }
 
+    # if key == "species":
+    #     print(key, st.session_state[key])
+    if st.session_state[key]["search"] == "":
+        def _get_label(label: any) -> str:
+            return str(label[0]) if isinstance(label, tuple) else str(label)
+
+        def _get_value(value: any) -> any:
+            return value[1] if isinstance(value, tuple) else value
+
+        st.session_state[key]["options"] = [
+            {
+                "label": _get_label(v),
+                "value": i,
+            }
+            for i, v in enumerate(initial_options)
+        ]
+        st.session_state[key]["options_real_type"] = [
+            _get_value(v) for v in initial_options]
+
     # everything here is passed to react as this.props.args
+    # print(f"DEFAULT ({key}): {default}")
     react_state = _get_react_component(
         options=st.session_state[key]["options"],
         clear_on_submit=clear_on_submit,
         placeholder=placeholder,
         label=label,
+        is_required=is_required,
         # react return state within streamlit session_state
         key=key_react,
+        value=default,
         **kwargs,
     )
+    # if key == "species":
+    #     print("REACT STATE", react_state)
 
     if react_state is None:
+        if on_change:
+            on_change(st.session_state[key]["result"])
         return st.session_state[key]["result"]
 
     interaction, value = react_state["interaction"], react_state["value"]
@@ -142,15 +176,21 @@ def st_searchbox(
         _process_search(search_function, key, value)
 
     if interaction == "submit":
+        # if key == "species":
+        #     print("SUBMITTED", st.session_state[key]["options_real_type"], value)
         st.session_state[key]["result"] = (
             st.session_state[key]["options_real_type"][value]
-            if "options_real_type" in st.session_state[key]
+            if "options_real_type" in st.session_state[key] and type(value) == int
             else value
         )
+        if on_change:
+            on_change(st.session_state[key]["result"])
         return st.session_state[key]["result"]
 
     if interaction == "reset":
         st.session_state[key]["result"] = default
+        if on_change:
+            on_change(default)
         return default
 
     # no new react interaction happened
